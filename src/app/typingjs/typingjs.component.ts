99a5668b693557c919c9aa0e596f1c286174e08a
Ashutosh Parija <asparija@microsoft.com>
2021-03-24 13:51:47 +0530

    adding three adjectives from team

--- src/app/typingjs/typingjs.component.ts
+++ src/app/typingjs/typingjs.component.ts
@@ -9,20 +9,15 @@ export class TypingjsComponent implements OnInit {
 
   wordsList = [];
   wordMessage = [
-    {name: 'Prafulla Sharma', words: ['Cricketer', 'Tutor', 'Life Long Learner']},
-    {name: 'Narendra Katlamudi', words: ['Highly energetic', 'Intelligent and Sensible person']},
-    {name: 'Rahul Tibdewal', words: ['Fun Happy Leader']},
-    {name: 'Ananta Gupta', words: ['Dynamic', 'Respecting', 'Perfectionist']},
-    {name: 'Kishore Chintalapati', words: ['Passionate', 'Always Smiling', ' Caring']},
-    {name: 'Harshavardhan Papudippu', words: ['Calm', 'Smart', 'Humble']},
-    {name: 'Anurag Agarwal', words: ['Versatile', 'Great Buddy']},
-    {name: 'Mayur Kale', words: ['Easy-Going', 'Smart', 'Hard working']},
-    {name: 'Gaurav Solanki', words: ['Young (looks that way) ;-)', 'Passionate', 'Energetic']},
-    {name: 'Vaibhav Gupta', words: ['Logical', 'Simple', 'Empathetic ']},
-    {name: 'Mukul Verma', words: ['Energetic', 'Compassionate', 'Optimistic']},
-    {name: 'Mohit', words: ['Strong', 'Positive', 'Hard working']},
-    {name: 'Jayneeta', words: ['Mentor', 'Motivation therapist']},
-    {name: 'Rahul Dev Burman', words: ['Organized', 'Friendly', 'Tech Savvy']}
+    {name: 'Parag', words: ['Mentor', ' Humble', ' Energetic']},
+    {name: 'Mansi', words: ['Inspiring', ' Innovative', ' Humble']},
+    {name: 'Navratan', words: ['Fun Happy Leader']},
+    {name: 'Akshita', words: ['Optimistic', ' Hard Working', ' Humble']},
+    {name: 'Ashutosh', words: ['Mentor', ' Inspiring', ' Passinate']},
+    {name: 'Nikunj', words: ['Passionate', ' Always Smiling', ' Caring']},
+    {name: 'Ankit', words: ['Bonsai', ' Commited', ' Coach']},
+    {name: 'Pranav', words: ['Strong', ' Positive', ' Hard working']},
+    {name: 'Ojasvin', words: ['Respecting', ' Mentor', ' Calm']}
   ];
   joinWords = ['says ', 'thinks that you are a ', 'feels', 'thinks of you as a', 'adds', 'calls you'];
 
